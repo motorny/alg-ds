a621a4c63fc747ee2621cce549cc3e3212fee154
maloxit <2rushan2@gmail.com>
2020-10-01 01:11:19 +0300

    Добавил один юнит тест

--- lab-1/unit-tests/test.cpp
+++ lab-1/unit-tests/test.cpp
@@ -74,3 +74,24 @@ TEST(SkipListAdd, TwoItemsSecondBigger_NoMemLeaks) {
 
   EXPECT_FALSE(_CrtMemDifference(&s3, &s1, &s2) && (_CrtMemDumpStatistics(&s3), true));
 }
+
+TEST(SkipListAdd, TreeItems_NoMemLeaks) {
+  _CrtMemState s1, s2, s3;
+
+  skipList* list;
+  void* value = NULL;
+  int key1 = 1, key2 = 2, key3 = 3;
+  char level1 = 1, level2 = 2, level3 = 3;
+
+  _CrtMemCheckpoint(&s1);
+
+  list = skipList_Get();
+  skipList_Add(list, value, key1, level1);
+  skipList_Add(list, value, key2, level2);
+  skipList_Add(list, value, key3, level3);
+  skipList_Free(list);
+
+  _CrtMemCheckpoint(&s2);
+
+  EXPECT_FALSE(_CrtMemDifference(&s3, &s1, &s2) && (_CrtMemDumpStatistics(&s3), true));
+}

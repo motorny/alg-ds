4369ffed26248518367d6a591ee9c54b06c93189
pauls-iv <pauls.iv@edu.spbstu.ru>
2020-10-04 23:04:47 +0300

    First try

---
+++ lab-1/mergelists.h
@@ -0,0 +1,34 @@
+#ifndef MERGELISTS_H_INCLUDED__
+#define MERGELISTS_H_INCLUDED__
+
+#pragma once
+
+// Two following structures form a "notebook" structure:
+// list_t - list for first letters, which also contain a pointer to sublist
+// sublist_t - list for words starting with this letter
+typedef struct subList_t {
+  char* word;
+  struct subList_t* next;
+} subList_t;
+
+typedef struct list_t {
+  char letter;
+  subList_t* words;
+  struct list_t* next;
+} list_t;
+
+list_t* CreateList(int* err);
+
+void DestroyList(list_t* p);
+
+subList_t* FindPlaceForWord(char* newWord, list_t* p);
+
+subList_t* AddWord(char* newWord, list_t* p, int* err);
+
+list_t* AddLetter(char newLetter, list_t* p, int* err);
+
+list_t* FindLetterMatch(list_t* list, char letter, int* flagMatchFound);
+
+list_t* MergeLists(list_t* list1, list_t* list2, int* err);
+
+#endif

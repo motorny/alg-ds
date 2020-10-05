9ebc8de7fa536e76f116f31b851f6f856b11a9df
AV3pml30 <aVorotnikov@users.noreply.github.com>
2020-10-05 22:07:46 +0300

    Update test.cpp
    Add memalloc test, when memory manager can't allocate memory.

--- lab-2/UnitTests/test.cpp
+++ lab-2/UnitTests/test.cpp
@@ -8,7 +8,7 @@
 #define SIZE_FOR_TWO_INFO_BLOCKS_AND_ONE_BYTE ((2 * sizeof(int) + sizeof(void*)) * 2 + 1)
 
 
-TEST(memalloc_AllocateBlockWithoutSplit_returnValipPtr) {
+TEST(memalloc_AllocateBlockWithoutSplit_returnValidPtr) {
   char memBlock[SIZE_FOR_TWO_INFO_BLOCKS_AND_ONE_BYTE];
   meminit(memBlock, SIZE_FOR_TWO_INFO_BLOCKS_AND_ONE_BYTE);
   char *allocated = (char*)memalloc(1);
@@ -25,7 +25,7 @@ TEST(memalloc_AllocateBlockWithoutSplit_returnValipPtr) {
 
 #define SIZE_FOR_THREE_INFO_BLOCKS_AND_TWO_BYTES ((2 * sizeof(int) + sizeof(void*)) * 3 + 2)
 
-TEST(memalloc_AllocateBlockWithSplit_returnValipPtr) {
+TEST(memalloc_AllocateBlockWithSplit_returnValidPtr) {
   char memBlock[SIZE_FOR_THREE_INFO_BLOCKS_AND_TWO_BYTES];
   meminit(memBlock, SIZE_FOR_THREE_INFO_BLOCKS_AND_TWO_BYTES);
   char *allocated = (char*)memalloc(1);
@@ -42,6 +42,22 @@ TEST(memalloc_AllocateBlockWithSplit_returnValipPtr) {
   ASSERT_EQ(*((int*)(memBlock + SHIFT(4, 2, 1))), 1);
   ASSERT_EQ(*((char**)(memBlock + SHIFT(5, 2, 1))), memBlock);
   ASSERT_EQ(*((int*)(memBlock + SHIFT(5, 3, 2))), 1);
+}
+
+TEST(memalloc_TryToAllocateBlockWithTooBigSize_returnNULL) {
+  char memBlock[SIZE_FOR_TWO_INFO_BLOCKS_AND_ONE_BYTE];
+  meminit(memBlock, SIZE_FOR_TWO_INFO_BLOCKS_AND_ONE_BYTE);
+  char *allocated = (char*)memalloc(1 + 1);
+
+  ASSERT_EQ(allocated, (char*)NULL);
+  // first block
+  ASSERT_EQ(*((int*)(memBlock)), 0);
+  ASSERT_EQ(*((char**)(memBlock + SHIFT(1, 0, 0))), memBlock + SHIFT(2, 1, 0));
+  ASSERT_EQ(*((int*)(memBlock + SHIFT(1, 1, 0))), 0);
+  // second block
+  ASSERT_EQ(*((int*)(memBlock + SHIFT(2, 1, 0))), 1);
+  ASSERT_EQ(*((char**)(memBlock + SHIFT(3, 1, 0))), memBlock);
+  ASSERT_EQ(*((int*)(memBlock + SHIFT(3, 2, 1))), 1);
 }
 
 TEST(memfree_FreeBlockWithoutUnite_returnNone) {

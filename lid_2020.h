c29f346ea0b3b1e75104ebb04653002819557aaf
justfriendlyneighbor <49965466+justfriendlyneighbor@users.noreply.github.com>
2020-10-08 18:03:50 +0300

    Update lid_2020.h

--- lid_2020.h
+++ lid_2020.h
@@ -1,27 +1,3 @@
-//#ifndef __POLYNOMIAL_H__
-//#define __POLYNOMIAL_H__
-//
-//#include <stdlib.h>
-//#include <stdbool.h>
-//
-//typedef struct poly_t poly;
-//poly  *polyCreate(size_t degree);
-//void   polyDestroy(poly *p);
-//void   polySetDegree(poly *p, size_t i, int a);
-//int    polyGetCoeff(poly *p, size_t i);
-//double polyEvaluale(poly *p, double x);
-//poly   *polyAdd(poly *p1, poly *p2);
-//poly   *polySub(poly *p1, poly *p2);
-//poly   *polyMul(poly *p1, poly *p2);
-//poly   *polyMulInt(poly *p, int c);
-//poly*  polyFrom(int n,...);
-//poly*  polyParse(const char *str);
-//void polyFormat(poly *poly,char * str);
-//poly*  polyRead(const char *str);
-//void   polyWrite(const char *filename, poly *poly);
-//char*  polyElement(poly* poly,int i);
-//#endif
-
 #ifndef __LinkedListsSort_H__
 #define __LinkedListsSort_H__
 typedef struct LIST LIST_T;

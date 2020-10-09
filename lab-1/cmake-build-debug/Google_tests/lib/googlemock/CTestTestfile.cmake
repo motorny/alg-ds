a215137b085a72126b5312102d04f153be2ba03a
alex <aleshka59000@gmail.com>
2020-10-09 09:39:22 +0300

    removed cmake-build-debug and .idea

--- lab-1/cmake-build-debug/Google_tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex4/Desktop/alg-ds/lab-1/Google_tests/lib/googlemock
-# Build directory: /home/alex4/Desktop/alg-ds/lab-1/cmake-build-debug/Google_tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- lab-1/cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex4/Desktop/alg-ds/lab-1/Google_tests/lib/googletest
-# Build directory: /home/alex4/Desktop/alg-ds/lab-1/cmake-build-debug/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

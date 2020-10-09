f31fdd8ca1d72efd831fbd0bfcecab9fae1ad111
reutov-as <reutovroma53@gmail.com>
2020-10-09 12:59:48 +0300

    Исправление замечаний

--- lab-1/Laba_1_26/Laba_1_26.c
+++ lab-1/Laba_1_26/Laba_1_26.c
@@ -5,107 +5,121 @@
 #include "Header.h"
 #pragma warning (disable: 4996)
 
+enum flag {
+	NO,
+	OK,
+	OK_2,
+	OK_3
+};
+
+//Указатель на начало списка
 list_p* head = NULL;
 
+//Сортировка списка по температуре 
 list_p* Sort(list_p* head) {
 	int min = 0;
 	list_p* list = (list_p*)malloc(sizeof(list_p));
-	list_p* tmp = (list_p*)malloc(sizeof(list_p)), * newhead = (list_p*)malloc(sizeof(list_p)), * tmp2 = (list_p*)malloc(sizeof(list_p)), * tmp3 = (list_p*)malloc(sizeof(list_p)), * tmp4 = (list_p*)malloc(sizeof(list_p));
+	list_p* currentlist = (list_p*)malloc(sizeof(list_p)), *newhead = (list_p*)malloc(sizeof(list_p)), * currentlist2 = (list_p*)malloc(sizeof(list_p)), * current = (list_p*)malloc(sizeof(list_p)), * nextfromcurrent = (list_p*)malloc(sizeof(list_p));
+	if (list == NULL || currentlist2 == NULL|| current == NULL|| nextfromcurrent == NULL)
+		return NULL;
 	list = head;
 	newhead = NULL;
 	while (list != NULL) {
+		//В начале каждого цикла принимаем за минимум текущую температуру и отталкиваемся от неё и проверяем следующий элемент
 		min = list->T;
-		tmp = list;
-		tmp3 = list;
+		currentlist = list;
+		current = list;
 		while (list->next != NULL) {
-			tmp2 = list;
-			list = tmp2->next;
+			currentlist2 = list;
+			list = currentlist2->next;
 			if (list->T > min) {
 				min = list->T;
-				tmp = list;
-				tmp4 = tmp2;
+				currentlist = list;
+				nextfromcurrent = currentlist2;
 			}
 			else if (list->T == min) {
-				if (list->year > tmp2->year) {
+				if (list->year > currentlist2->year) {
 					min = list->T;
-					tmp = list;
-					tmp4 = tmp2;
+					currentlist = list;
+					nextfromcurrent = currentlist2;
 				}
-				else if (list->year == tmp2->year) {
-					if (list->month > tmp2->month) {
+				else if (list->year == currentlist2->year) {
+					if (list->month > currentlist2->month) {
 						min = list->T;
-						tmp = list;
-						tmp4 = tmp2;
+						currentlist = list;
+						nextfromcurrent = currentlist2;
 					}
-					else	if (list->month == tmp2->month) {
-						if (list->day > tmp2->day) {
+					else	if (list->month == currentlist2->month) {
+						if (list->day > currentlist2->day) {
 							min = list->T;
-							tmp = list;
-							tmp4 = tmp2;
+							currentlist = list;
+							nextfromcurrent = currentlist2;
 						}
 					}
 				}
 			}
 		}
-		if (tmp3->T == min && tmp == tmp3) {
-			head = tmp3->next;
-			tmp3->next = newhead;
-			newhead = tmp3;
+		//Данное условие нужно, если первый элемент и есть минимум
+		if (current->T == min && currentlist == current) {
+			head = current->next;
+			current->next = newhead;
+			newhead = current;
 		}
 		else {
-			tmp4->next = tmp->next;
-			head = tmp3;
-			tmp->next = newhead;
-			newhead = tmp;
+			nextfromcurrent->next = currentlist->next;
+			head = current;
+			currentlist->next = newhead;
+			newhead = currentlist;
 		}
 		list = head;
 	}
 	return newhead;
 }
 
+//Печать информации тех дней, температура которых меньше нуля
 void Print(list_p* list) {
-	int flag = 0;
+	enum flag n = NO;
 	while (list != NULL) {
 		if (list->T < 0) {
 			if (list->day < 10)
-				flag = 1;
+				n = OK;
 			if (list->month < 10)
-				flag = 2;
+				n = OK_2;
 			if (list->day < 10 && list->month < 10)
-				flag = 3;
-			if (flag == 0)
+				n = OK_3;
+			if (n == NO)
 				printf("%d.%d.%d  %d\n", list->day, list->month, list->year, list->T);
-			if (flag == 1)
+			if (n == OK)
 				printf("0%d.%d.%d  %d\n", list->day, list->month, list->year, list->T);
-			if (flag == 2)
+			if (n == OK_2)
 				printf("%d.0%d.%d  %d\n", list->day, list->month, list->year, list->T);
-			if (flag == 3)
+			if (n == OK_3)
 				printf("0%d.0%d.%d  %d\n", list->day, list->month, list->year, list->T);
 			list = list->next;
-			flag = 0;
 		}
 		else list = list->next;
 	}
 }
 
+//Поиск заданной температуры в списке и вывод информации о ней, если она есть в списке
 int FindTemp(list_p* list, int t, int fl) {
-	int flag = 0, count = 0;
+	enum flag n = NO, count = 0;
 	while (list != NULL) {
 		if (list->T == t) {
 			if (fl != 0) {
 				if (list->day < 10)
-					flag = 1;
+					n = OK;
 				if (list->month < 10)
-					flag = 2;
+					n = OK_2;
 				if (list->day < 10 && list->month < 10)
-					flag = 3;
-				if (flag == 0)
+					n = OK_3;
+				if (n == NO)
 					printf("%d.%d.%d  %d\n", list->day, list->month, list->year, list->T);
-				if (flag == 1)
+				if (n == OK)
 					printf("0%d.%d.%d  %d\n", list->day, list->month, list->year, list->T);
-				if (flag == 2)
+				if (n == OK_2)
 					printf("%d.0%d.%d  %d\n", list->day, list->month, list->year, list->T);
-				if (flag == 3)
+				if (n == OK_3)
 					printf("0%d.0%d.%d  %d\n", list->day, list->month, list->year, list->T);
 				count++;
 			}
@@ -116,11 +130,11 @@ int FindTemp(list_p* list, int t, int fl) {
 			printf("Заданной температуры нет в списке\n");
 			break;
 		}
-		flag = 0;
 	}
 	return -1;
 }
 
+//Конвертация символьного типа в численный
 int WhatDay(char str[]) {
 	int x, y;
 	x = str[0] - 48;
@@ -168,19 +182,21 @@ int WhatTemperature(char str[]) {
 		return x;
 }
 
+//Добавление нового узла в список
 list_p* Add(list_p* head, char str[]) {
-	list_p* tmp = (list_p*)malloc(sizeof(list_p));
-	if (tmp == NULL)
+	list_p* next = (list_p*)malloc(sizeof(list_p));
+	if (next == NULL)
 		return NULL;
-	tmp->day = head->day;
-	tmp->month = head->month;
-	tmp->year = head->year;
-	tmp->T = head->T;
-	tmp->next = head;
-	head = tmp;
+	next->day = head->day;
+	next->month = head->month;
+	next->year = head->year;
+	next->T = head->T;
+	next->next = head;
+	head = next;
 	return head;
 }
 
+//Инициализация элемента списка по входящим данным
 list_p* Init(list_p* head, char str[]) {
 	head->day = WhatDay(str);
 	head->month = WhatMonth(str);
@@ -189,6 +205,7 @@ list_p* Init(list_p* head, char str[]) {
 	return head;
 }
 
+//Уничтожение списка
 list_p* Destroy(list_p* head) {
 	while (head != NULL) {
 		list_p* p = head;
@@ -198,8 +215,12 @@ list_p* Destroy(list_p* head) {
 	return head;
 }
 
+
+//Создание списка так, что после каждого добавления он остается отсортированным
 list_p* CreateList(FILE* f) {
 	list_p* list = (list_p*)malloc(sizeof(list_p));
+	if (list == NULL)
+		return NULL;
 	char* a;
 	char str[50];
 	int n = 0;
@@ -222,34 +243,35 @@ list_p* CreateList(FILE* f) {
 		}
 		head = Init(head, str);
 		list = head;
-		head = Add(head, str);
+		list = Sort(list);
+		head = Add(list, str);
 	}
+	list = Sort(list);
 	head = list;
 	return head;
 }
 
-//int main(void) {
-//	FILE* f;
-//	int t = 0, fl = 0;
-//	setlocale(LC_CTYPE, "russian");
-//	f = fopen("temp.txt", "r");
-//	if (f == NULL) {
-//		printf("Не удалось открыть файл");
-//		return -1;
-//	}
-//	head = CreateList(f);
-//	head = Sort(head);
-//	Print(head);
-//	printf("\n");
-//	printf("Введите температуру, которую хотите найти\n");
-//	scanf_s("%d", &t);
-//	printf("\nВот, что удалось найти:\n");
-//	if (head == NULL) {
-//		printf("Список пуст\n");
-//	}
-//	else
-//		fl = FindTemp(head, t, fl);
-//	head = Destroy(head);
-//	fclose(f);
-//	return 0;
-//}
+int main(void) {
+	FILE* f;
+	int t = 0, fl = 1;
+	setlocale(LC_CTYPE, "russian");
+	f = fopen("temp.txt", "r");
+	if (f == NULL) {
+		printf("Не удалось открыть файл");
+		return -1;
+	}
+	head = CreateList(f);
+	Print(head);
+	printf("\n");
+	printf("Введите температуру, которую хотите найти\n");
+	scanf_s("%d", &t);
+	printf("\nВот, что удалось найти:\n");
+	if (head == NULL) {
+		printf("Список пуст\n");
+	}
+	else
+		fl = FindTemp(head, t, fl);
+	head = Destroy(head);
+	fclose(f);
+	return 0;
+}

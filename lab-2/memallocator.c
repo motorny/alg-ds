bc785e31618d41f6b7af36408804034596d6038a
Sairsey <vladimir.parusov.vp1@gmail.com>
2020-10-11 22:58:09 +0300

    Added file that passed tests with 100/100

--- lab-2/memallocator.c
+++ lab-2/memallocator.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "memallocator.h"
 
-char *gMemory = NULL;
+char* gMemory = NULL;
 int gSize = 0;
 
 typedef struct NodeHeader NodeHeader;
@@ -10,7 +10,7 @@ struct NodeHeader
 {
   int size;
   int magic_number; // for checking validness of pointer
-  NodeHeader *Next;
+  NodeHeader* Next;
 };
 
 typedef struct NodeEnder
@@ -18,18 +18,20 @@ typedef struct NodeEnder
   int size;
 } NodeEnder;
 
-NodeHeader *FreeSpaceList;
+NodeHeader* FreeSpaceList;
 
 // Init memory system with memory block pMemory.
 int meminit(void* pMemory, int size)
 {
-  gMemory = pMemory;
+  if (size < memgetminimumsize())
+    return 0;
+  gMemory = (char*)pMemory;
   gSize = size;
-  FreeSpaceList = pMemory;
+  FreeSpaceList = (NodeHeader*)pMemory;
   FreeSpaceList->Next = NULL;
   FreeSpaceList->magic_number = 3030;
   FreeSpaceList->size = gSize - sizeof(NodeHeader) - sizeof(NodeEnder);
-  ((NodeEnder *)(gMemory + sizeof(NodeHeader) + FreeSpaceList->size))->size = FreeSpaceList->size;
+  ((NodeEnder*)(gMemory + sizeof(NodeHeader) + FreeSpaceList->size))->size = FreeSpaceList->size;
   return 1;
 }
 
@@ -38,7 +40,7 @@ int meminit(void* pMemory, int size)
 void* memalloc(int size)
 {
   // good block
-  NodeHeader **Cur = NULL;
+  NodeHeader** Cur = NULL;
   NodeHeader* NewBlock;
   int Cur_size;
   int Best_size = -1;
@@ -76,19 +78,20 @@ void* memalloc(int size)
   }
 
   // change Ender
-  ((NodeEnder *)((char *)*Cur + sizeof(NodeHeader) + Cur_size))->size = Cur_size - (sizeof(NodeHeader) + size + sizeof(NodeEnder));
+  ((NodeEnder*)((char*)*Cur + sizeof(NodeHeader) + Cur_size))->size = Cur_size - (sizeof(NodeHeader) + size + sizeof(NodeEnder));
 
   // change Header
-  *Cur = ((char*)*Cur + sizeof(NodeHeader) + size + sizeof(NodeEnder) );
+  *Cur = (NodeHeader*)((char*)*Cur + sizeof(NodeHeader) + size + sizeof(NodeEnder));
   (*Cur)->size = Cur_size - (sizeof(NodeHeader) + size + sizeof(NodeEnder));
-  (*Cur)->Next = NULL;
+  (*Cur)->magic_number = 3030;
+  (*Cur)->Next = NewBlock->Next;
 
   // add new Block
   NewBlock->size = size;
   NewBlock->magic_number = 3030;
   NewBlock->Next = NULL;
-  ((NodeEnder *)((char *)NewBlock + sizeof(NodeHeader) + size))->size = -1; // -1 means we cannot fuse blocks
-  return (char *)NewBlock + sizeof(NodeHeader);
+  ((NodeEnder*)((char*)NewBlock + sizeof(NodeHeader) + size))->size = -1; // -1 means we cannot fuse blocks
+  return (char*)NewBlock + sizeof(NodeHeader);
 }
 
 int memgetminimumsize()
@@ -104,52 +107,74 @@ int memgetblocksize()
 // Free memory previously allocated by memalloc
 void memfree(void* p)
 {
-  NodeHeader *Header;
-  NodeEnder *PrevEnder;
-  NodeEnder *Ender;
+  NodeHeader* Header;
+  NodeHeader* NextHeader;
+  NodeEnder* NextEnder;
+  NodeEnder* PrevEnder;
+  NodeEnder* Ender;
 
-  if ((char *)p - gMemory < 0 || (char*)p - gMemory > gSize)
+  if ((char*)p - gMemory < 0 || (char*)p - gMemory >= gSize)
     return;
   if (p == NULL)
     return;
 
-  Header = (char*)p - sizeof(NodeHeader);
+  Header = (NodeHeader*)((char*)p - sizeof(NodeHeader));
   if (Header->magic_number != 3030)
     return;
 
-  PrevEnder = (char*)p - sizeof(NodeHeader) - sizeof(NodeEnder);
-  Ender = (char*)p + Header->size;
-
-
-  if ((char *)PrevEnder < gMemory || PrevEnder->size == -1) // means we cannot concat ourself with previous block
-  { 
+  PrevEnder = (NodeEnder*)((char*)p - sizeof(NodeHeader) - sizeof(NodeEnder));
+  Ender = (NodeEnder*)((char*)p + Header->size);
+  NextHeader = (NodeHeader*)((char*)Ender + sizeof(NodeEnder));
+  if ((char*)PrevEnder < gMemory || PrevEnder->size == -1) // means we cannot concat ourself with previous block
+  {
     Header->Next = FreeSpaceList; // just add block
     Ender->size = Header->size;
-    FreeSpaceList = Header; 
+    FreeSpaceList = Header;
   }
   else // if we can concat with previous block
   {
-    NodeHeader* PrevHeader = (char*)p - sizeof(NodeHeader) - sizeof(NodeEnder) - PrevEnder->size - sizeof(NodeHeader);
+    NodeHeader* PrevHeader = (NodeHeader*)((char*)p - sizeof(NodeHeader) - sizeof(NodeEnder) - PrevEnder->size - sizeof(NodeHeader));
     PrevHeader->size += sizeof(NodeHeader) + sizeof(NodeEnder) + Header->size;
     Ender->size = PrevHeader->size;
     Header = PrevHeader;
   }
+
+  if ((char*)NextHeader - gMemory < gSize && NextHeader->magic_number == 3030)
+  {
+    NextEnder = (NodeEnder*)((char*)NextHeader + sizeof(NodeHeader) + NextHeader->size);
+    if (NextEnder->size != -1) // concat with next one
+    {
+      NodeHeader **tmp = &FreeSpaceList; 
+      Header->size += sizeof(NodeHeader) + sizeof(NodeEnder) + NextHeader->size;
+      NextHeader->magic_number = 0;
+      NextEnder->size = Header->size;
+      while (*tmp != NULL && (*tmp)!= NextHeader)
+        tmp = &(*tmp)->Next;
+      if (*tmp == NextHeader)
+        *tmp = (*tmp)->Next;
+    }
+  }
+
+
+
   return;
 }
 
 // You can implement memory leak checks here
 void memdone()
 {
-  NodeHeader *tmp = gMemory;
+  NodeHeader* tmp = (NodeHeader*)gMemory;
   while ((char*)tmp - gMemory < gSize)
   {
-    NodeEnder *End = (char *)tmp + sizeof(NodeHeader) + tmp->size;
+    NodeEnder* End = (NodeEnder*)((char*)tmp + sizeof(NodeHeader) + tmp->size);
     if (End->size == -1)
-    { 
+    {
+      #ifdef SOBAKA
       fprintf(stderr, "return my memory, sobaka");
+      #endif
       break;
     }
-    tmp = (char *)End + sizeof(NodeEnder);
+    tmp = (NodeHeader*)((char*)End + sizeof(NodeEnder));
   }
   gMemory = NULL;
   gSize = 0;

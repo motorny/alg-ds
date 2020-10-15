35793f59df32f279ce0326be9dad129b474f8c6d
KiselevaAleksandra <72768554+KiselevaAleksandra@users.noreply.github.com>
2020-10-15 15:50:25 +0300

    Add files via upload
    Добавила проверку корректности работы функции calloc

--- lab-1/test.cpp
+++ lab-1/test.cpp
@@ -18,19 +18,21 @@ TEST(TestSortTwo, TestSortTwoMovingElements) {
   test.prev = NULL;
   list_t* test1 = (list_t*)calloc(1, sizeof(list_t));
   list_t* test2 = (list_t*)calloc(1, sizeof(list_t));
-  strcpy_s(test1->str, "b");
-  strcpy_s(test2->str, "a");
-  test1->prev = test.prev;
-  test.prev = test1;
-  test2->prev = test.prev;
-  test.prev = test2;
-  list_t* firstTest = &test;
-  SortTwo(firstTest->prev);
-  ASSERT_STREQ(firstTest->prev->str, "a");
-  ASSERT_STREQ(firstTest->prev->prev->str, "b");
-  ASSERT_TRUE(firstTest->prev->prev->prev == NULL);
-  free(test1);
-  free(test2);
+  if (test1 != NULL && test2 != NULL) {
+    strcpy_s(test1->str, "b");
+    strcpy_s(test2->str, "a");
+    test1->prev = test.prev;
+    test.prev = test1;
+    test2->prev = test.prev;
+    test.prev = test2;
+    list_t* firstTest = &test;
+    SortTwo(firstTest->prev);
+    ASSERT_STREQ(firstTest->prev->str, "a");
+    ASSERT_STREQ(firstTest->prev->prev->str, "b");
+    ASSERT_TRUE(firstTest->prev->prev->prev == NULL);
+    free(test1);
+    free(test2);
+  }
 }
 
 TEST(TestSortTwo, TestSortTwoNoMovingElements) {
@@ -38,18 +40,20 @@ TEST(TestSortTwo, TestSortTwoNoMovingElements) {
   test.prev = NULL;
   list_t* test1 = (list_t*)calloc(1, sizeof(list_t));
   list_t* test2 = (list_t*)calloc(1, sizeof(list_t));
-  strcpy_s(test1->str, "a");
-  strcpy_s(test2->str, "b");
-  test1->prev = test.prev;
-  test.prev = test1;
-  test2->prev = test.prev;
-  test.prev = test2;
-  list_t* firstTest = &test;
-  SortTwo(firstTest->prev);
-  ASSERT_STREQ(firstTest->prev->str, "b");
-  ASSERT_TRUE(firstTest->prev->prev == NULL);
-  free(test1);
-  free(test2);
+  if (test1 != NULL && test2 != NULL) {
+    strcpy_s(test1->str, "a");
+    strcpy_s(test2->str, "b");
+    test1->prev = test.prev;
+    test.prev = test1;
+    test2->prev = test.prev;
+    test.prev = test2;
+    list_t* firstTest = &test;
+    SortTwo(firstTest->prev);
+    ASSERT_STREQ(firstTest->prev->str, "b");
+    ASSERT_TRUE(firstTest->prev->prev == NULL);
+    free(test1);
+    free(test2);
+  }
 }
 
 TEST(TestSortList, TestSortListSorted) {
@@ -59,29 +63,31 @@ TEST(TestSortList, TestSortListSorted) {
   list_t* test2 = (list_t*)calloc(1, sizeof(list_t));
   list_t* test3 = (list_t*)calloc(1, sizeof(list_t));
   list_t* test4 = (list_t*)calloc(1, sizeof(list_t));
-  strcpy_s(test1->str, "c");
-  strcpy_s(test2->str, "d");
-  strcpy_s(test3->str, "a");
-  strcpy_s(test4->str, "b");
-  test1->prev = test.prev;
-  test.prev = test1;
-  test2->prev = test.prev;
-  test.prev = test2;
-  test3->prev = test.prev;
-  test.prev = test3;
-  test4->prev = test.prev;
-  test.prev = test4;
-  list_t* first= &test;
-  SortList(first);
-  ASSERT_STREQ(first->prev->str, "a");
-  ASSERT_STREQ(first->prev->prev->str, "b");
-  ASSERT_STREQ(first->prev->prev->prev->str, "c");
-  ASSERT_STREQ(first->prev->prev->prev->prev->str, "d");
-  ASSERT_TRUE(first->prev->prev->prev->prev->prev == NULL);
-  free(test1);
-  free(test2);
-  free(test3);
-  free(test4);
+  if (test1 != NULL && test2 != NULL && test3 != NULL && test4 != NULL) {
+    strcpy_s(test1->str, "c");
+    strcpy_s(test2->str, "d");
+    strcpy_s(test3->str, "a");
+    strcpy_s(test4->str, "b");
+    test1->prev = test.prev;
+    test.prev = test1;
+    test2->prev = test.prev;
+    test.prev = test2;
+    test3->prev = test.prev;
+    test.prev = test3;
+    test4->prev = test.prev;
+    test.prev = test4;
+    list_t* first = &test;
+    SortList(first);
+    ASSERT_STREQ(first->prev->str, "a");
+    ASSERT_STREQ(first->prev->prev->str, "b");
+    ASSERT_STREQ(first->prev->prev->prev->str, "c");
+    ASSERT_STREQ(first->prev->prev->prev->prev->str, "d");
+    ASSERT_TRUE(first->prev->prev->prev->prev->prev == NULL);
+    free(test1);
+    free(test2);
+    free(test3);
+    free(test4);
+  }
 }
 
 int main(void) {

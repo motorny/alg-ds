391cdb04c50199c584084a662ec2f987de6e75b3
justfriendlyneighbor <49965466+justfriendlyneighbor@users.noreply.github.com>
2020-10-15 19:04:59 +0300

    Add files via upload

---
+++ alt_lib_2020.h
@@ -0,0 +1,11 @@
+#ifndef __AltLinkedListsSort_H__
+#define __AltLinkedListsSort_H__
+typedef struct LIST LIST_T;
+
+//Not public functions
+//void Split(LIST_T* head, LIST_T** first, LIST_T** second);
+//LIST_T * Merge(LIST_T *A, LIST_T *B);
+
+void Sort(LIST_T **head);
+
+#endif 

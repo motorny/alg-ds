80a94a7b33baffa3d29f6e83051fcbb63ae2d35c
Ugin10 <72548462+Ugin10@users.noreply.github.com>
2020-10-15 15:54:01 +0300

    Update get_truth.c

--- lab-1/test/get_truth.c
+++ lab-1/test/get_truth.c
@@ -1,19 +1,13 @@
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 #include "join.h"
+#include "create_test_list.h"
+CR
 int main() {
-	struct List list = { NULL,0 };
-	struct Data data1;
-	struct Data data2;
-	struct Data data3;
-	data1.int_field = 5;
-	data2.int_field = 7;
-	data3.int_field = 10;
-	Add(&list, &data1);
-	Add(&list, &data2);
-	Add(&list, &data3);
-	assert(Get(&list, 0).int_field == 5);
-	assert(Get(&list, 1).int_field == 7);
-	assert(Get(&list, 2).int_field == 10);
+  struct List* li = CreateTestList(5, 7, 10);
+  assert(Get(li, 0).int_field == 5);
+  assert(Get(li, 1).int_field == 7);
+  assert(Get(li, 2).int_field == 10);
 	return 0;
 }

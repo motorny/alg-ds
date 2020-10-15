090cf03694a20eed7539ae990bbe27ccc70044bc
Ugin10 <72548462+Ugin10@users.noreply.github.com>
2020-10-15 15:56:54 +0300

    Update join_notempty_empty.c

--- lab-1/test/join_notempty_empty.c
+++ lab-1/test/join_notempty_empty.c
@@ -1,19 +1,12 @@
 #include <assert.h>
 #include "join.h"
+#include "create_test_list.h"
+CR
 int main() {
-	struct List list1 = { NULL,0 };
+	struct List* list1 = CreateTestList(5, 7, 10);
 	struct List list2 = { NULL,0 };
 	struct List list3;
-	struct Data data1;
-	struct Data data2;
-	struct Data data3;
-	data1.int_field = 5;
-	data2.int_field = 7;
-	data3.int_field = 10;
-	Add(&list1, &data1);
-	Add(&list1, &data2);
-	Add(&list1, &data3);
-	list3 = Join(&list1, &list2);
+	list3 = Join(list1, &list2);
 	assert(list3.size == 3);
 	assert(list3.first->data.int_field == 5);
 	assert(list3.first->next->data.int_field == 7);

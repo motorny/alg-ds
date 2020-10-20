5949bb017ade7dc56992cdc0084efeebeb55ed40
Nutsheil <redchenko.eyu@edu.spbstu.ru>
2020-10-21 00:06:56 +0300

    Task2 complete 96/100

---
+++ lab-2/memallocator.h
@@ -0,0 +1,26 @@
+#ifndef __MEMALLOCATOR_H__E71A34CB
+#define __MEMALLOCATOR_H__E71A34CB
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int meminit(void *pMemory, int size);
+
+void memdone();
+
+void* memalloc(int size);
+
+void memfree(void *p);
+
+int memgetminimumsize();
+
+int memgetblocksize();
+
+int CheakMemoryLeaks();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

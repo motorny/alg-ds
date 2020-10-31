976f6c5d1dba99b9a0a4574d2fdc7838346d359a
Lindrella <72521483+lindrella@users.noreply.github.com>
2020-10-31 23:13:59 +0300

    Update LabA23.h

--- LabA23.h
+++ LabA23.h
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#define MAX_LETTER_COUNT 100
+#define SAME_LENGTH_WITH_HEAD -2
+#define SAME_LENGTH -1
+#define NEW_HEAD 0
+#define ADD_AFTER_SELECTED_LIST 1
 
 typedef struct list {
-	char word[100];
+	char word[MAX_LETTER_COUNT];
 	struct list* next;
 	unsigned CountOfLet;
 }list;

ebbb21a3309960eb9f8c541ad0d5f86fe3a3f1bf
mihalich436 <62470513+mihalich436@users.noreply.github.com>
2020-11-05 18:30:22 +0300

    Update StackArr.c
    1. Убрал все ассерты
    2. Перенес все return на строку ниже

--- lab-1/StackArr.c
+++ lab-1/StackArr.c
@@ -1,13 +1,14 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include "StackArr.h"
 #pragma warning(disable: 4996)
 
 stackArr_t* CreateStackArr(void) {
 	stackArr_t* stack = (stackArr_t*)malloc(sizeof(stackArr_t));
-	if(stack==NULL) return NULL;
+	if(stack==NULL) {
+		return NULL;
+	}
 	stack->last = -1;
 	stack->delta = 10;
 	stack->size = stack->delta;
@@ -19,39 +20,54 @@ stackArr_t* CreateStackArr(void) {
 	return stack;
 }
 void DestroyStackArr(stackArr_t* stack) {
-	if (stack == NULL) return;
+	if (stack == NULL) {
+		return;
+	}
 	if (stack->array != NULL) free(stack->array);
 	free(stack);
 }
 
 void ResizeStackArr(stackArr_t* stack) {
 	int* p;
-	assert(stack);
+	if(stack==NULL){
+		return;
+	}
 	stack->size += stack->delta;
 	p = (int*)realloc(stack->array, stack->size * sizeof(int));
-	assert(p);
+	if(p==NULL){
+		stack->size -= stack->delta;
+		return;
+	}
 	stack->array = p;
 }
 
 void PushArr(stackArr_t* stack, int data) {
-	assert(stack);
+	if(stack==NULL){
+		return;
+	}
 	stack->last++;
 	if (stack->last >= stack->size) ResizeStackArr(stack);
 	stack->array[stack->last] = data;
 }
 
 int PopArr(stackArr_t* stack) {
-	assert(stack);
-	assert(stack->array);
-	if (stack->last < 0) return 0;
+	if(stack==NULL || stack->array){
+		return;
+	}
+	if (stack->last < 0) {
+		return 0;
+	}
 	stack->last--;
 	return stack->array[stack->last + 1];
 }
 
 int LastArr(stackArr_t* stack) {
-	assert(stack);
-	assert(stack->array);
-	if (stack->last < 0) return 0;
+	if(stack==NULL || stack->array){
+		return;
+	}
+	if (stack->last < 0) {
+		return 0;
+	}
 	return stack->array[stack->last];
 }
 

bcffef8d7e3b2f5c46b53fbb1752ae57f2f678e3
Jolkas <ivalexsay@gmail.com>
2020-11-16 17:06:49 +0300

    [3630102/90002]_Ivakin_Aleksei_B12

---
+++ lab-2/main.cpp
@@ -0,0 +1,20 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include "memallocator.h"
+
+typedef struct Head {
+  int size;
+  int check;
+  struct Head* next;
+}Head;
+
+int main(void) {
+  char ptr[30];
+  int whaaat;
+  int size = 30 - memgetminimumsize();
+  meminit(ptr, 30);
+  char* buf = (char*)memalloc(size);
+  whaaat = ((Head*)(buf - sizeof(Head)))->check;
+  return 0;
+}

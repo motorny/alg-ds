8f01fafd5b3f5e54c16f7152964ebfee2e45e523
ilyuha10 <74465625+ilyuha10@users.noreply.github.com>
2020-11-20 19:08:39 +0300

    Update list.c

--- lab-1/list.c
+++ lab-1/list.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+    #include<stdio.h>
 #include"list.h"
 #include<stdlib.h>
 #include<assert.h>
@@ -17,42 +17,55 @@ void ListDestroy(list_t* list) {
 	free(list);
 }
 
-char* Merge(list_t* list, int n) {
-	int k = 0;
-	int mem = maxLength * n;
+int Length(char s[][maxLength], int n) {
+	int len = 0;
+	int i = 0;
+	for (; i < n; i++) {
+		len += (int)strlen(s[i]);
+	}
+	return len;
+}
+
+char* Merge(list_t* list, int len) {
+	int lenStr = 0, count = 0;
 	if (!list)
 		return NULL;
-	char* str = malloc(mem * sizeof(char));
-	assert(str);
+	char* str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return NULL;
 	while (list) {
-		k += sprintf(str + k, "%s", list->str);
-
+		lenStr = (int)strlen(list->str);
+		memcpy(str + count, list->str, lenStr);
 		list = list->next;
-
+		count += lenStr;
 	}
+	str[count] = '\0';
 	return str;
 }
 
-list_t* InsertNode(char word[]) {
+list_t* InsertStrToList(char word[]) {
 
 	list_t* elem = (list_t*)malloc(sizeof(list_t));
-	assert(elem);
+	if (!elem)
+		return NULL;
 
 	elem->str = word;
 	elem->next = NULL;
 	return elem;
 }
 
-list_t* Insert(char s[][maxLength], int n) {
+list_t* InsertTextToList(char s[][maxLength], int n) {
 	int i = 0;
 	list_t* list = NULL;
-	
-	list = InsertNode(s[i]);
-	assert(list);
+	if (n < 1)
+		return NULL;
+	list = InsertStrToList(s[i]);
+	if (!list)
+		return NULL;
 	i++;
 	list_t* e = list;
 	while (i < n) {
-		list->next = InsertNode(s[i]);
+		list->next = InsertStrToList(s[i]);
 		list = list->next;
 		i++;
 	}
@@ -62,3 +75,5 @@ list_t* Insert(char s[][maxLength], int n) {
 
 
 
+
+    

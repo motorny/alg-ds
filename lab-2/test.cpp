abd532849338ceede9c1ab08bf3abad343ad83a4
DimDimch <60793570+DimDimch@users.noreply.github.com>
2020-12-05 02:27:24 +0300

    3630102/90003_Serdyukov Dmitry_C4

--- lab-2/test.cpp
+++ lab-2/test.cpp
@@ -1,57 +1,66 @@
 #include "pch.h"
-
+#include <stdio.h>
 extern "C" {
-#include "../LAB_B/memallocator.h"
-#include "../LAB_B/lab_b_11.cpp"
+#include "..\LAB_C\graph.c"
 }
 
-TEST(MeminitFunction, Not_Enough_Space) {
-    char a[10];
-    int i = meminit(a, 10);
-    EXPECT_EQ(i, -1);
+FILE* fp = NULL;
+
+FILE* TestGraphCreate(void) {
+    fp = fopen("TestGraph.txt", "w");
+    fprintf(fp, "5\n0 4\n1 3\n1 4");
+    fclose(fp);
+    fp = fopen("TestGraph.txt", "r");
+    return fp;
 }
 
-TEST(MeminitFunction, Enough_Space) {
-    char a[30];
-    int i = meminit(a, 30);
-    EXPECT_EQ(i, 0);
+TEST(NullTest, ReadGraph_Test) {
+    int check;
+    graph_t g;
+    check = ReadGraph(NULL, stdin);
+    EXPECT_EQ(check, -1);
+    check = ReadGraph(&g, NULL);
+    EXPECT_EQ(check, -1);
 }
+TEST(BigTest, ReadGraph_Test) {
+    graph_t g;
+    ReadGraph(&g, TestGraphCreate());
+    fclose(fp);
+    EXPECT_EQ(g.size, 5);
+    EXPECT_EQ(g.head->data, 0);
+    EXPECT_EQ(g.head->next->data, 1);
+    EXPECT_EQ(g.head->next->next->data, 2);
+    EXPECT_EQ(g.head->next->next->next->data, 3);
+    EXPECT_EQ(g.head->next->next->next->next->data, 4);
 
-TEST(AllocFunction, WithoutFun_meminit) {
-    void* p = memalloc(1);
-    EXPECT_EQ(p, (void*)0);
+    EXPECT_EQ(g.head->links->ptr->data, 4);
+    EXPECT_EQ(g.head->next->links->ptr->data, 3);
+    EXPECT_EQ(g.head->next->links->next->ptr->data, 4);
+    DestroyGraph(&g);
 }
-
-TEST(AllocFunction, Not_Enough_Space) {
-    char a[30];
-    meminit(a, 30);
-    void* p = memalloc(30);
-    EXPECT_EQ(p, (void*)0);
+TEST(NullTest, BFS_Test) {
+    int check;
+    graph_t g;
+    check = BFS(NULL, stdin);
+    EXPECT_EQ(check, -1);
+    check = BFS(&g, NULL);
+    EXPECT_EQ(check, -1);
 }
+TEST(BigTest, BFS_Test) {
+    graph_t g;
+    FILE* f = fopen("TestAnswer.txt", "w");
+    int a, b, c, d;
+    ReadGraph(&g, TestGraphCreate());
+    fclose(fp);
+    BFS(&g, f);
+    fclose(f);
 
-TEST(AllocFunction, Enough_Space) {
-    char a[200];
-    meminit(a, 30);
-    void* p = memalloc(30);
-    EXPECT_TRUE(p != (void*)0);
-}
-
-TEST(AllocFunction, Main_Test) {
-    char a[200];
-    meminit(a, 30);
-    void* p = memalloc(30);
-    void* q = memalloc(5);
-    EXPECT_TRUE(a + SIZE_PART_BLOCK == p);
-    EXPECT_TRUE(*(int*)(a + SIZE_PART_BLOCK + 30 - sizeof(int)) == -30);
-    EXPECT_TRUE(a + SIZE_PART_BLOCK + 30 + SIZE_PART_BLOCK == q);
-}
-
-TEST(FreeFunction, Main_Test) {
-    char a[200];
-    meminit(a, 30);
-    void* p = memalloc(30);
-    memfree(p);
-    void* q = memalloc(5);
-    memfree(q);
-    EXPECT_TRUE(q == p);
+    fopen("TestAnswer.txt", "r");
+    fscanf(f, "%d %d %d %d", &a, &b, &c, &d);
+    EXPECT_EQ(a, 0);
+    EXPECT_EQ(b, 4);
+    EXPECT_EQ(c, 1);
+    EXPECT_EQ(d, 3);
+    fclose(f);
+    DestroyGraph(&g);
 }

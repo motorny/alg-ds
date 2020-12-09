3df3b5bfe258264e7aa63203efc7636b9a08b798
magliy <68062800+magliy@users.noreply.github.com>
2020-12-10 00:33:02 +0600

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,50 +1,129 @@
 #include "pch.h"
-#include "../Lab A/list.c"
+#include "../LabBMalloc.2/memallocator.c"
 
-TEST(findPlace, SameHours) {
-	Node N1 = { "11.06.2011", "Ivanov A.", 24, NULL };
-	Node N2 = { "11.05.2012", "Ivanov B.", 24, NULL };
-	Node N3 = { "10.01.2001", "Ivanov G.", 24, NULL };
-	Node N4 = { "14.10.2004", "Ivanov I.", 24, END_OF_LIST };
-	N1.pPrev = &N2;
-	N2.pPrev = &N3;
-	N3.pPrev = &N4;
-
-	Node newNode = { "11.06.2015", "Ivanov F.", 24, NULL };
-
-	EXPECT_EQ(findPlace(&N1, &newNode), &N3);
+TEST(memInit, NULLpointer_Minus1)
+{
+	char* a = NULL;
+	ASSERT_EQ(-1, meminit(a, 100));
+	memdone();
 }
 
-TEST(findPlace, DifferentHours) {
-	Node N1 = { "11.06.2011", "Ivanov A.", 39, NULL };
-	Node N2 = { "11.05.2012", "Ivanov B.", 24, NULL };
-	Node N3 = { "10.01.2001", "Ivanov G.", 11, NULL };
-	Node N4 = { "14.10.2004", "Ivanov I.", 5, END_OF_LIST };
-	N1.pPrev = &N2;
-	N2.pPrev = &N3;
-	N3.pPrev = &N4;
-
-	Node newNode = { "11.06.2015", "Ivanov F.", 4, NULL };
-
-	EXPECT_EQ(findPlace(&N1, &newNode), nullptr);
+TEST(memInit, NULLsize_Minus1)
+{
+	char a[100];
+	ASSERT_EQ(-1, meminit(a, 0));
+	memdone();
 }
 
-TEST(findLastNode, NullPointer) {
-	EXPECT_EQ(findLastNode(NULL), nullptr);
-}
-TEST(findLastNode, OneNode) {
-	Node N1 = { "11.06.2011", "Ivanov A.", 39, END_OF_LIST };
-	EXPECT_EQ(findLastNode(&N1), &N1);
+TEST(memInit, validArguments_1)
+{
+	char a[100];
+	ASSERT_EQ(1, meminit(a, 100));
+	memdone();
 }
 
-TEST(findLastNode, MoreThanOneNodes) {
-	Node N1 = { "11.06.2011", "Ivanov A.", 39, NULL };
-	Node N2 = { "11.05.2012", "Ivanov B.", 24, NULL };
-	Node N3 = { "10.01.2001", "Ivanov G.", 11, NULL };
-	Node N4 = { "14.10.2004", "Ivanov I.", 5, END_OF_LIST };
-	N1.pPrev = &N2;
-	N2.pPrev = &N3;
-	N3.pPrev = &N4;
+TEST(memInit, sizeLessMin_Minus1)
+{
+	char a[10];
+	ASSERT_EQ(-1, meminit(a, 10));
+	memdone();
+}
 
-	EXPECT_EQ(findLastNode(&N1), &N4);
+TEST(memInit, reinitialization_Minus1)
+{
+	char a[20];
+	char b[20];
+	meminit(a, 20);
+	ASSERT_EQ(-1, meminit(b, 20));
+	memdone();
+}
+
+TEST(memalloc, NULLMinusBytes_NULL)
+{
+	char a[100];
+	meminit(a, 100);
+	ASSERT_EQ(NULL, memalloc(0));
+	ASSERT_EQ(NULL, memalloc(-1));
+	memdone();
+}
+
+TEST(memalloc, PositiveBytes_ValidPointer)
+{
+	char a[100];
+	char* b, * c;
+	meminit(a, 100);
+	b = (char*)memalloc(20);
+	c = (char*)memalloc(20);
+
+	ASSERT_EQ(&a[8], b);
+	ASSERT_EQ(&a[40], c);
+	memdone();
+}
+
+TEST(memalloc, notEnoughMemory_NULL)
+{
+	char a[20];
+	char* b;
+	meminit(a, 20);
+	b = (char*)memalloc(40);
+
+	ASSERT_EQ(NULL, b);
+	memdone();
+}
+
+TEST(memfree, freeFirstAndMallocSecond_Memory)
+{
+	char a[25];
+	char* p1, * p2;
+	meminit(a, 25);
+	p1 = (char*)memalloc(1);
+	memfree(p1);
+	p2 = (char*)memalloc(1);
+	ASSERT_EQ(&a[8], p2);
+	memdone();
+}
+
+TEST(memfree, mergeBlocksRight)
+{
+	char a[100];
+	char* d, * b, * c;
+	meminit(a, 100);
+	b = (char*)memalloc(20);
+	c = (char*)memalloc(20);
+	d = (char*)memalloc(10);
+	memfree(b);
+	memfree(c);
+	ASSERT_EQ(52, *(int*)(a));
+	memdone();
+}
+
+TEST(memfree, mergeBlocksLeft)
+{
+	char a[100];
+	meminit(a, 100);
+	char* d, * b, * c;
+	meminit(a, 100);
+	b = (char*)memalloc(20);
+	c = (char*)memalloc(20);
+	d = (char*)memalloc(10);
+	memfree(c);
+	memfree(b);
+	ASSERT_EQ(52, *(int*)(a));
+	memdone();
+}
+
+TEST(memfree, mergeBlocksTwoSides)
+{
+	char a[100];
+	meminit(a, 100);
+	char* d, * b, * c;
+	meminit(a, 100);
+	b = (char*)memalloc(20);
+	c = (char*)memalloc(20);
+	d = (char*)memalloc(10);
+	memfree(b);
+	memfree(d);
+	memfree(c);
+	ASSERT_EQ(88, *(int*)(a));
+	memdone();
 }

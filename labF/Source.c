6420ddb01bdebfec7e9d5a62c4a33292d004f231
levs2001 <50678585+levs2001@users.noreply.github.com>
2020-12-09 22:18:20 +0300

    All files added

---
+++ labF/Source.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"SectionTree.h"
+
+int main(void) {
+	int leftBorder = 0, rightBorder = 0;
+	NODE* root = NULL;
+	NODE** interMass = NULL;
+	ConsoleTree(&root);
+	printTreePreOrder(root, "root", 0);
+	
+	printf("Enter section that should be intered\n");
+	scanf("%d %d", &leftBorder, &rightBorder);
+	interMass = FindInterSegm(root, leftBorder, rightBorder);
+	PrintSectMass(interMass);
+	free(interMass);
+	
+	DestroyTree(&root);
+	
+	return 0;
+}

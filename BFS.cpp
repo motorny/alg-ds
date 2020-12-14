9d5c1b4a2bb73a08e275c3c472479ae9a03e01bc
AleksandrShvartz <63416774+AleksandrShvartz@users.noreply.github.com>
2020-12-15 01:46:45 +0300

    Update BFS.cpp
    Исправил согласно указаниям.

--- BFS.cpp
+++ BFS.cpp
@@ -1,18 +1,19 @@
 #include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "vld.h" //for finding memory limits in debug
+//#include "vld.h" //for finding memory limits in debug
 #define SIZEBUF 100
+#define ERROR -1
 typedef struct Queue {
 	Queue* next;
 	Queue* prev;
 	int data;
 } Queue_t;
 
-void rec(int** mass,int n,Queue_t** start,Queue_t** end) {
+void rec(int** mass, int n, Queue_t** start, Queue_t** end) {
 	if ((*start)->next != *end) {//while queue is not empty
-		
-		int j =(*end)->prev->data;
+
+		int j = (*end)->prev->data;
 		(*end)->prev->prev->next = *end;
 		Queue_t* tmp = (*end)->prev->prev;
 		free((*end)->prev);
@@ -23,9 +24,9 @@ void rec(int** mass,int n,Queue_t** start,Queue_t** end) {
 		{
 			if (mass[j][i] && i != j && !mass[i][n]) {
 				Queue_t* q = (Queue_t*)malloc(sizeof(Queue_t));
-				q->next = (*start)->next; 
+				q->next = (*start)->next;
 				q->prev = *start;
-				((*start)->next)->prev=q;
+				((*start)->next)->prev = q;
 				(*start)->next = q;
 				mass[i][n] = 1;
 				q->data = i;
@@ -34,71 +35,51 @@ void rec(int** mass,int n,Queue_t** start,Queue_t** end) {
 		}
 		rec(mass, n, start, end);
 	}
-	/*else {
-		for (size_t i = 0; i < n; i++)
-		{
-			if (!mass[i][n]) {
-				Queue_t* q = (Queue_t*)malloc(sizeof(Queue_t));
-				(*start)->next = q;
-				(*end)->prev = q;
-				q->next =*end;
-				q->prev = *start;
-				q->data = i;
-				mass[i][n] = 1;
-				rec(mass, n, start, end);
-				break;
-		  }	 
-		} 
 
-	}*/
 }
-int** createArr(int n) {
-
-  }
-int main(){
-int n = 0,o=0;
-int a=0, b=0;
-char buf[SIZEBUF];
-char c;
-int flag = 1;
+int main() {
+	int n = 0, o = 0;
+	int a = 0, b = 0;
+	char buf[SIZEBUF];
+	char c;
+	int flag = 1;
 	//scanf("%d", &n);
-//FILE* f=fopen("file.txt", "rt");
-//fscanf(f, "%i", &n);
-char str[SIZEBUF];
+    //FILE* f=fopen("file.txt", "rt");
+    //fscanf(f, "%i", &n);
+	char str[SIZEBUF];
 	while ((c = (char)getchar()) != '\n') {
 		str[o++] = c;
-
-		}
+	}
 	str[o] = 0;
 	n = atoi(str);
-	
-	
-	int** mass = (int**)malloc(n*sizeof(int*));
+
+
+	int** mass = (int**)malloc(n * sizeof(int*));
 	for (size_t i = 0; i < n; i++)
 	{
-	   mass[i]=(int*)malloc((n+1)*sizeof(int));
-	   for (size_t j = 0; j <= n; j++)
-	   {
-		   if (i != j ||i==n)		   //write zeros in the special colomn(index n)
-			   mass[i][j] = 0;	 //
-		   else 
-			   mass[i][j] = 1;			 // to see have we visited vertex yet or not
-	   }
+		mass[i] = (int*)malloc((n + 1) * sizeof(int));
+		for (size_t j = 0; j <= n; j++)
+		{
+			if (i != j || i == n)		   //write zeros in the special colomn(index n)
+				mass[i][j] = 0;	 //
+			else
+				mass[i][j] = 1;			 // to see have we visited vertex yet or not
+		}
 	}
-	
+
 	flag = 1;
 	o = 0;
 	while ((c = (char)getchar()) != EOF) {
 		buf[o++] = c;
-		if (c == ' '||c=='\n') {
+		if (c == ' ' || c == '\n') {
 			buf[o] = 0;
-			if(flag%2)
-	    	a = atoi(buf);
+			if (flag % 2)
+				a = atoi(buf);
 			else {
-				b= atoi(buf);
+				b = atoi(buf);
 				mass[a][b] = 1;
 				mass[b][a] = 1;
-			//	printf("%i %i ",a,b);
+				//	printf("%i %i ",a,b);
 			}
 			o = 0;
 			flag++;
@@ -107,9 +88,11 @@ char str[SIZEBUF];
 	}
 	//fclose(f);
 
-	Queue_t* start=(Queue_t*)malloc(sizeof(Queue_t));
+	Queue_t* start = (Queue_t*)malloc(sizeof(Queue_t));
 	Queue_t* end = (Queue_t*)malloc(sizeof(Queue_t));
 	Queue_t* q = (Queue_t*)malloc(sizeof(Queue_t));
+	if (!start || !end || !q)
+		return ERROR;
 	start->next = q;
 	start->prev = NULL;
 	end->prev = q;
@@ -118,8 +101,7 @@ char str[SIZEBUF];
 	q->prev = start;
 	q->data = 0;
 	mass[0][n] = 1;
-
-	rec(mass,n,&start,&end);
+	rec(mass, n, &start, &end);
 
 	for (size_t i = 0; i < n; i++)
 	{

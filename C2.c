1d4c4e60fea1efc56532047a0fd0ad45f9a9edad
pauls-iv <pauls.iv@edu.spbstu.ru>
2020-12-16 14:23:03 +0300

    Gtests added.

--- C2.c
+++ C2.c
@@ -1,29 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "C2.h"
 
 #pragma warning (disable:4996)
-
-/*
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-*/
-
-typedef enum {
-  WHITE,
-  GRAY,
-  BLACK,
-} color_t;
-
-typedef struct edge_t {
-  struct node_t* node;
-  struct edge_t* next;
-} edge_t;
-
-typedef struct node_t {
-  int edgesQ;
-  color_t color;
-  edge_t* edges;
-} node_t;
 
 node_t* CreateGraph(int n, int* err) {
   node_t* nodes = (node_t*)malloc(sizeof(node_t) * n);
@@ -128,14 +105,14 @@ void PrintEdges(node_t* nodes, int n) {
   }
 }
 
-void DFS(int* i, int* ans, int nodeNum, node_t* nodes, int n) {
+static void _DFS(int* i, int* ans, int nodeNum, node_t* nodes, int n) {
   edge_t *edgeCur = nodes[nodeNum].edges;
 
   ans[(*i)++] = nodeNum;
   nodes[nodeNum].color = GRAY;
   while (edgeCur != NULL) {
     if (edgeCur->node->color == WHITE)
-      DFS(i, ans, edgeCur->node - nodes, nodes, n);
+      _DFS(i, ans, edgeCur->node - nodes, nodes, n);
     edgeCur = edgeCur->next;
   }
   nodes[nodeNum].color = BLACK;
@@ -144,11 +121,11 @@ void DFS(int* i, int* ans, int nodeNum, node_t* nodes, int n) {
 void RunDFS(int* ans, node_t* nodes, int n) {
   int i = 0, nodeNum = 0;
 
-  DFS(&i, ans, nodeNum, nodes, n);
+  _DFS(&i, ans, nodeNum, nodes, n);
 
   for (nodeNum = 0; nodeNum < n; nodeNum++) {
-    if (nodes[nodeNum].color = WHITE)
-      DFS(&i, ans, nodeNum, nodes, n);
+    if (nodes[nodeNum].color == WHITE)
+      _DFS(&i, ans, nodeNum, nodes, n);
   }
 }
 
@@ -159,31 +136,4 @@ void PrintDFS(int* ans, int n) {
   for (i = 1; i < n; i++)
     printf(" %i", ans[i]);
   printf("\n");
-}
-
-int main(void) {
-  int err = 0;
-  int n = 0;
-  //FILE* fp = fopen("test2.txt", "r");
-  node_t* nodes = FillFromStream(stdin, &n, &err);
-  int *ans = (int*)malloc(sizeof(int) * n);
-
-  /*
-  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
-  */
-
-  //PrintEdges(nodes, n);
-  if (ans != NULL) {
-    RunDFS(ans, nodes, n);
-    PrintDFS(ans, n);
-  }
-
-  DestroyGraph(nodes, n);
-  free(ans);
-
-  //fclose(fp);
-
-  return 0;
 }

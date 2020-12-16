d1309a25a791cc94bae832b8c30ec880aaf564cc
justfriendlyneighbor <49965466+justfriendlyneighbor@users.noreply.github.com>
2020-12-16 07:21:13 +0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,221 +1,45 @@
 #include "pch.h"
-#include "lib_2020.c"
 
-LIST_T* InitSplittedListFirstHalf() {
-	LIST_T * List, *go, *tmp;
-	List = (LIST_T*)malloc(sizeof(LIST_T));
-	go = List;
-	go->prev = NULL;
-	go->str = "First test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Second test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Third test message";
-	go->next = NULL;
-	return List;
+extern "C" {
+#include "C:/Users/Lenovo/source/repos/AlgC4/C4.c"
 }
-LIST_T* InitSplittedListSecondHalf() {
-	LIST_T * List, *go, *tmp;
-	List = (LIST_T*)malloc(sizeof(LIST_T));
-	go = List;
-	go->prev = NULL;
-	go->str = "k";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Afdio";
-	go->next = NULL;
-	return List;
+TEST(InitGraph, normalWork_returnValidPointer) {
+	graph gr;
+	InitGraph(&gr);
+	ASSERT_TRUE(gr.size == 0);
+	ASSERT_TRUE(gr.next == NULL);
 }
-LIST_T* InitSplittedListSecondHalfSorted() {
-	LIST_T * List, *go, *tmp;
-	List = (LIST_T*)malloc(sizeof(LIST_T));
-	go = List;
-	go->prev = NULL;
-	go->str = "Afdio";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "k";
-	go->next = NULL;
-	return List;
+TEST(PushGraph, nullPtr_returnOne) {
+	graph* gr = NULL;
+	ASSERT_TRUE(PushGraph(gr, 1) == 1);
 }
-LIST_T* InitSortedList()
-{
-	LIST_T * List, *go, *tmp;
-	List = (LIST_T*)malloc(sizeof(LIST_T));
-	go = List;
-	go->prev = NULL;
-	go->str = "Afdio";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "First test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Second test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Third test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "k";
-	go->next = NULL;
-	return List;
+TEST(PushGraph, normalWorkOneNode_returnZero) {
+	graph* gr;
+	InitGraph(gr);
+	ASSERT_TRUE(PushGraph(gr, 1) == 0);
+	ASSERT_TRUE(gr->next[0]== 1);
 }
-LIST_T* InitNotSortedList()
-{
-	LIST_T * List, *go, *tmp;
-	List = (LIST_T*)malloc(sizeof(LIST_T));
-	go = List;
-	go->prev = NULL;
-	go->str = "First test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Second test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Third test message";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "k";
-	go->next = (LIST_T*)malloc(sizeof(LIST_T));
-	tmp = go;
-	go = go->next;
-	go->prev = tmp;
-	go->str = "Afdio";
-	go->next = NULL;
-	return List;
+TEST(PushGraph, normalWorkTwoNode_returnZero) {
+	graph* gr;
+	InitGraph(gr);
+	ASSERT_TRUE(PushGraph(gr, 1) == 0);
+	ASSERT_TRUE(PushGraph(gr, 2) == 0);
+	ASSERT_TRUE(gr->next[0] == 1);
+	ASSERT_TRUE(gr->next[1] == 2);
 }
-TEST(Merge_BothNULLLists_ReturnNULL)
-{
-	LIST_T *ListToMerge, *ListToCheck, *List1, *List2;
-	ListToCheck = NULL;
-	List1 = NULL;
-	List2 = NULL;
-	ListToMerge = Merge(List1, List2);
-	EXPECT_EQ(ListToMerge, ListToCheck);
+TEST(PopGraph, nullPtr_returnMinusOne) {
+	graph* gr = NULL;
+	ASSERT_TRUE(PopGraph(gr) == -1);
 }
-TEST(Merge_NULLAndNotNULLLists_ReturnValList)
-{
-	LIST_T *ListToMerge, *List1, *List2;
-	List1 = InitSplittedListFirstHalf();
-	List2 = NULL;
-	ListToMerge = Merge(List1, List2);
-	EXPECT_EQ(ListToMerge, List1);
+TEST(PopGraph, emptyQueue_returnMinusOne) {
+	graph* gr;
+	InitGraph(gr);
+	ASSERT_TRUE(PopGraph(gr) == -1);
 }
-TEST(Merge_NotNULLLists_ReturnValList) 
-{
-	LIST_T *ListToMerge, *ListToCheck, *List1, *List2;
-	int ListLength = 5;
-	ListToCheck = InitSortedList();
-	List1 = InitSplittedListFirstHalf();
-	List2 = InitSplittedListSecondHalfSorted();
-	ListToMerge=Merge(List1,List2);
-	for (int i=0;i<ListLength-1;i++) 
-	{
-		EXPECT_EQ(0, strcmp((*ListToMerge).str, (*ListToCheck).str));
-		ListToMerge->next->prev = ListToMerge;
-		ListToMerge = ListToMerge->next;
-		ListToCheck->next->prev = ListToCheck;
-		ListToCheck = ListToCheck->next;
-	}
-	EXPECT_EQ(0, strcmp((*ListToMerge).str, (*ListToCheck).str));
+TEST(PopGraph, normalWork_returnCorrectNumber) {
+	graph* gr;
+	InitGraph(gr);
+	PushGraph(gr, 1);
+	int result = PopGraph(gr);
+	ASSERT_EQ(result, 1);
 }
-TEST(Split_NULLList_ReturnNULLs) 
-{
-	LIST_T *ListToSplit, *List1, *List2;
-	ListToSplit = NULL;
-	Split(ListToSplit, &List1, &List2);
-	EXPECT_EQ(ListToSplit,List1);
-	EXPECT_EQ(ListToSplit, List1);
-}
-TEST(Split_NotNULLList_ReturnValValue)
-{
-	LIST_T *ListToSplit,*List1ToCheck, *List2ToCheck, *List1, *List2;
-	int List1Length = 3, List2Length = 2;
-	ListToSplit = InitNotSortedList();
-	List1ToCheck = InitSplittedListFirstHalf();
-	List2ToCheck = InitSplittedListSecondHalf();
-	Split(ListToSplit, &List1, &List2);
-	for (int i = 0; i < List1Length - 1; i++)
-	{
-		EXPECT_EQ(0, strcmp((*List1).str, (*List1ToCheck).str));
-		List1->next->prev = List1;
-		List1 = List1->next;
-		List1ToCheck->next->prev = List1ToCheck;
-		List1ToCheck = List1ToCheck->next;
-	}
-	EXPECT_EQ(0, strcmp((*List1).str, (*List1ToCheck).str));
-	for (int i = 0; i < List2Length - 1; i++)
-	{
-		EXPECT_EQ(0, strcmp((*List2).str, (*List2ToCheck).str));
-		List2->next->prev = List2;
-		List2 = List2->next;
-		List2ToCheck->next->prev = List2ToCheck;
-		List2ToCheck = List2ToCheck->next;
-	}
-	EXPECT_EQ(0, strcmp((*List2).str, (*List2ToCheck).str));
-}
-TEST(Sort_SortedList_ListRemainSorted)
-{
-	LIST_T *List1, *List2;
-	int ListLength = 5;
-	List1 = InitSortedList();
-	List2 = InitSortedList();
-	Sort(&List1);
-	for (int i = 0; i < ListLength - 1; i++)
-	{
-		EXPECT_EQ(0, strcmp((*List1).str, (*List2).str));
-		List1->next->prev = List1;
-		List1 = List1->next;
-		List2->next->prev = List2;
-		List2 = List2->next;
-	}
-	EXPECT_EQ(0, strcmp((*List1).str, (*List2).str));
-}
-TEST(Sort_NotSortedList_ListBecameSorted)
-{
-	LIST_T *List1,*List2;
-	int ListLength = 5;
-	List1 = InitNotSortedList(); 
-	List2 = InitSortedList();
-	Sort(&List1);
-	for (int i = 0; i < ListLength-1; i++)
-	{
-		EXPECT_EQ(0,strcmp((*List1).str,(*List2).str));
-		List1->next->prev = List1;
-		List1 = List1->next;
-		List2->next->prev = List2;
-		List2 = List2->next;
-	}
-	EXPECT_EQ(0, strcmp((*List1).str, (*List2).str));
-}
-TEST(Sort_NULLList_ListRemainNULL)
-{
-	LIST_T *List=NULL;
-	Sort(&List);
-	EXPECT_EQ(NULL,List);
-}
-

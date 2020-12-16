e2322500b9a521f3870d26b9258d41f800e824c4
Stasyo <72510396+Stasyo@users.noreply.github.com>
2020-12-16 11:35:50 +0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,92 +1,46 @@
 #include "pch.h"
-#include"../MallocFree/MallocFree.c"
+#include"../GraphBFS/Graph.c"
 
-
-TEST(meminit, return1) 
-{
-
-	EXPECT_EQ(1, meminit(NULL, 1));
-
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
 }
 
-TEST(meminit, NegativeSizeReturn1)
-{
-	void* ptr = malloc(memgetminimumsize() + 1);
-
-	EXPECT_EQ(1, meminit(ptr, -1));
-	
-
-	free(ptr);
-}
-
-TEST(meminit, CorrectDataReturn1)
-{
-	void* ptr = malloc(memgetminimumsize() + 1);
-
-	EXPECT_EQ(1, meminit(ptr, memgetminimumsize() + 1));
-
-	free(ptr);
-}
-
-TEST(memalloc, return0) 
-{
-	void* ptr = malloc(memgetminimumsize() + 1);
-	meminit(ptr, memgetminimumsize() + 1);
-
-	EXPECT_EQ(0, memalloc(-1));
-
-	free(ptr);
-}
-
-TEST(memalloc, returnValidPointer)
-{
-	void* ptr = malloc(memgetminimumsize() + 1);
-	meminit(ptr, memgetminimumsize() + 1);
-
-	void* p1 = memalloc(1);
-	EXPECT_TRUE(p1 != NULL);
-
-	memfree(p1);
-	free(ptr);
-}
-
-TEST(memalloc, returnNULL)
-{
-	void* ptr = malloc(memgetminimumsize() + 1);
-	meminit(ptr, memgetminimumsize() + 1);
-	void* p1 = memalloc(2);
-	EXPECT_TRUE(p1 == NULL);
-	memfree(p1);
-	free(ptr);
-}
-
-TEST(memfree, freeNULL) 
-{
-    void* p = malloc(memgetminimumsize() + sizeof(int));
-    meminit(p, memgetminimumsize() + sizeof(int));
-    char* s = (char*)memalloc(memgetminimumsize());
-    memfree(s);
-    memdone();
-    free(p);
-}
-
-TEST(memfree, freeNotOurMemory) 
-{
-    char* p = (char*)malloc(memgetminimumsize() + sizeof(int));
-    meminit(p, memgetminimumsize() + sizeof(int));
-    memfree(p + 100);
-    memdone();
-    free(p);
-}
-
-TEST(memfree, freeNotOurAllocatedMemory)
-{
-    char* p = (char*)malloc(10 * memgetminimumsize());
-    meminit(p, 10 * memgetminimumsize());
-    char* s = (char*)memalloc(1);
-    memfree(p + 5 * memgetminimumsize() + sizeof(char));
-    memdone();
-    free(p);
+TEST(createGraph, NegSize_ReturnNULL) {
+    ASSERT_EQ(createGraph(-1), (struct Graph*)NULL);
 }
 
 
+TEST(createQueue, normalWork_returnValidPointer) {
+    struct queue* queue = createQueue();
+    ASSERT_TRUE(queue != NULL);
+    ASSERT_TRUE(queue->head == NULL);
+    ASSERT_TRUE(queue->tail == NULL);
+    destroyQueue(queue);
+}
+
+
+TEST(pushQueue, normalWorkTwoNode_returnSUCCESS) {
+   struct queue* queue = createQueue();
+    ASSERT_EQ(pushQueue(queue, 1), SUCCESS);
+    ASSERT_EQ(pushQueue(queue, 2), SUCCESS);
+    ASSERT_EQ(queue->head->vertex, 1);
+    ASSERT_EQ(queue->tail->vertex, 2);
+    destroyQueue(queue);
+}
+TEST(popQueue, nullPtr_returnFAIL) {
+    struct queue* queue = NULL;
+    ASSERT_EQ(popQueue(queue), FAIL);
+}
+TEST(popQueue, emptyQueue_returnFAIL) {
+    struct queue* queue = createQueue();
+    ASSERT_EQ(popQueue(queue), FAIL);
+    destroyQueue(queue);
+}
+TEST(popQueue, normalWork_returnCorrectNumber) {
+    struct queue* queue = createQueue();
+    pushQueue(queue, 10);
+    int result = popQueue(queue);
+    ASSERT_EQ(result, 10);
+    destroyQueue(queue);
+}

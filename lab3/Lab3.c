c9f7c42c8910c3da7cab3e0099148132cb4a6dec
APorokh <73940702+APorokh@users.noreply.github.com>
2021-02-14 12:05:40 +0300

    Add files via upload

--- lab3/Lab3.c
+++ lab3/Lab3.c
@@ -1,29 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "Lab3.h"
 #pragma warning (disable:4996)
-
-#define FAIL 0
-#define SUCCESS 1
-
-#define HAVE 1
-#define NOT_HAVE 0
-
-typedef struct {
-	int* arr;
-	int size;
-	int head;
-	int tail;
-} queue_t;
-
-typedef struct {
-	int* edges;
-	int numE;
-} node_t;
-
-typedef struct {
-	node_t* tops;
-	int numT;
-} adjacency_list_t;
 
 void ReadAdjacencyList(adjacency_list_t* list) {
 	int i;
@@ -122,58 +98,4 @@ int BreadthFirstSearch(adjacency_list_t* list, queue_t* q) {
 
 	free(temp);
 	return SUCCESS;
-}
-int main(void) {
-	int i, j;
-	int ind;
-	adjacency_list_t list;
-	queue_t q;
-
-
-	ind = scanf("%i", &list.numT);
-
-
-	if (CreateQ(&q, list.numT) == FAIL)
-		return 0;
-	list.tops = (node_t*)calloc(list.numT, sizeof(node_t));
-
-	if (list.tops == NULL) {
-		DeleteQ(&q);
-		return 0;
-	}
-
-	for (i = 0; i < list.numT; i++) {
-		list.tops[i].edges = (int*)calloc(list.numT, sizeof(int));
-
-		if (list.tops[i].edges == NULL) {
-			for (j = 0; j < i; j++) {
-				free(list.tops[j].edges);
-			}
-
-			free(list.tops);
-			DeleteQ(&q);
-			return 0;
-		}
-	}
-
-
-
-	ReadAdjacencyList(&list);
-	BreadthFirstSearch(&list, &q);
-
-	for (i = 0; i < q.tail; i++)
-		printf("%i ", q.arr[i]);
-
-
-	for (i = 0; i < list.numT; i++) {
-		if (list.tops[i].edges != NULL) {
-			free(list.tops[i].edges);
-
-		}
-	}
-
-
-	free(list.tops);
-	DeleteQ(&q);
-	return 0;
 }
--- lab3/Lab3.h
+++ lab3/Lab3.h
@@ -1,1 +1,44 @@
+#ifndef LABC3_H__INCLUDED__
+#define LABC3_H__INCLUDED__
 #pragma once
+
+#ifdef cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define FAIL 0
+#define SUCCESS 1
+
+#define HAVE 1
+#define NOT_HAVE 0
+
+	typedef struct {
+		int* arr;
+		int size;
+		int head;
+		int tail;
+	} queue_t;
+
+	typedef struct {
+		int* edges;
+		int numE;
+	} node_t;
+
+	typedef struct {
+		node_t* tops;
+		int numT;
+	} adjacency_list_t;
+
+	void ReadAdjacencyList(adjacency_list_t* list);
+	int CreateQ(queue_t* q, int size);
+	int AddInTail(queue_t* q, int nextTop);
+	int GetFromHead(queue_t* q, int* oldEl);
+	void DeleteQ(queue_t* q);
+
+	int BreadthFirstSearch(adjacency_list_t* list, queue_t* q);
+
+#endif //LABC3_H__INCLUDED__ #pragma once

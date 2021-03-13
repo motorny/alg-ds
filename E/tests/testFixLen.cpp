a034d6f57f87138bd765df3b33d8d30cc6acbde6
lyphrowny <strukov.as@edu.spbstu.ru>
2021-03-13 17:49:06 +0300

    renamed FixHeight to FixLen, since the function it tests is finLen

--- E/tests/testFixLen.cpp
+++ E/tests/testFixLen.cpp
@@ -2,11 +2,11 @@
 
 #include "../BinTree.h"
 
-class FixHeight: public ::testing::Test {
+class FixLen: public ::testing::Test {
 protected:
     Node_t *root = nullptr;
 
-    ~FixHeight() override { freeTree(root); }
+    ~FixLen() override { freeTree(root); }
 
     Node_t* insertNodes(const std::vector<int>& values) {
         for (int i : values)
@@ -15,45 +15,45 @@ protected:
     }
 };
 
-TEST_F(FixHeight, 1Node) {
+TEST_F(FixLen, 1Node) {
     root = insertNodes({1});
     ASSERT_EQ(root->len, 0);
 }
 
-TEST_F(FixHeight, Root1Left) {
+TEST_F(FixLen, Root1Left) {
     root = insertNodes({1, 0});
     ASSERT_EQ(root->left->len, 0);
     ASSERT_EQ(root->len, 1);
 }
 
-TEST_F(FixHeight, Root1Right) {
+TEST_F(FixLen, Root1Right) {
     root = insertNodes({0, 1});
     ASSERT_EQ(root->right->len, 0);
     ASSERT_EQ(root->len, 1);
 }
 
-TEST_F(FixHeight, Root1Left1Right) {
+TEST_F(FixLen, Root1Left1Right) {
     root = insertNodes({1, 0, 2});
     ASSERT_EQ(root->right->len, 0);
     ASSERT_EQ(root->left->len, 0);
     ASSERT_EQ(root->len, 2);
 }
 
-TEST_F(FixHeight, Root1Left2W1Right3W) {
+TEST_F(FixLen, Root1Left2W1Right3W) {
     root = insertNodes({11, 10, 100});
     ASSERT_EQ(root->right->len, 0);
     ASSERT_EQ(root->left->len, 0);
     ASSERT_EQ(root->len, 5);
 }
 
-TEST_F(FixHeight, Root2Left3W2W) {
+TEST_F(FixLen, Root2Left3W2W) {
     root = insertNodes({101, 100, 10});
     ASSERT_EQ(root->len, 5);
     ASSERT_EQ(root->left->len, 2);
     ASSERT_EQ(root->left->left->len, 0);
 }
 
-TEST_F(FixHeight, Root2Right2W3W) {
+TEST_F(FixLen, Root2Right2W3W) {
     root = insertNodes({10, 11, 100});
     ASSERT_EQ(root->len, 5);
     ASSERT_EQ(root->right->len, 3);

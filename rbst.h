d9bee8cb14d1123104fbe7a61239467f3bc83767
fab9073 <72733482+fab9073@users.noreply.github.com>
2021-03-14 20:46:12 +0300

    Update rbst.h

--- rbst.h
+++ rbst.h
@@ -1,1 +1,34 @@
 #pragma once
+#pragma warning(disable:4996)
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+
+typedef struct node
+{
+    int key;
+    int size;
+    struct node* left;
+    struct node* right;
+}node;
+
+struct node* node_create(int key);
+
+struct node* node_find(struct node* root, int key);
+
+int node_getsize(struct node* root);
+
+void node_fixsize(struct node* root);
+
+struct node* rot_right(struct node* ptr_node);
+
+struct node* rot_left(struct node* ptr_node);
+
+struct node* insertnode(struct node* ptr_node, int key);
+
+struct node* insert(struct node* ptr_node, int key);
+
+struct node* join(struct node* ptr_node1, struct node* ptr_node2);
+
+struct node* remove_node(struct node* node, int key);

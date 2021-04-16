61bbfdc95d6810cfb6e947cade567563e46fd31f
lyphrowny <strukov.as@edu.spbstu.ru>
2021-04-16 17:06:07 +0300

    modified main.c to get console input

--- I/main.c
+++ I/main.c
@@ -1,14 +1,33 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 #include "23Tree.h"
 
 int main() {
 
-    Node_t* root = newNode(1);
+    Node_t* root = NULL;
+    int c = getchar(), number;
 
-    root = insertTerminate(root, 3);
+    while (c != EOF){
+        scanf("%d", &number);
+        switch(c){
+            case 'a':
+                root = insertNode(root, number);
+                break;
+            case 'f':
+                printf(findNode(root, number) == NULL ? "no\n" : "yes\n");
+                break;
+            case 'r':
+                root = deleteNode(root, number);
+                break;
+            default:
+                freeTree(root);
+                return 0;
+        }
 
-    free(root);
+        if((c = getchar()) == '\n')
+            c = getchar();
+    }
+    freeTree(root);
 
     return 0;
 }

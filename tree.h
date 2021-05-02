7d800469926dad63ab3cc2f1127e2d2d9a0680dd
Ugin10 <72548462+Ugin10@users.noreply.github.com>
2021-05-02 18:10:25 +0300

    Add files via upload

---
+++ tree.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning (disable:4996)
+#define EMPTY (-1)
+typedef struct Tree {
+  struct Tree* left;
+  struct Tree* middle;
+  struct Tree* right;
+  int left_value;
+  int right_value;
+  int max_child;
+  struct Tree* left1;
+  struct Tree* right1;
+} Tree_t;
+void freeN(Tree_t* node);
+void freeTr(Tree_t* root);
+Tree_t* newN(int val);
+Tree_t* insert(Tree_t* root, int value);
+Tree_t* insertN(Tree_t* root, int value);
+Tree_t* deleteT(Tree_t* root, int value);
+Tree_t* deleteN(Tree_t* root, int value);
+Tree_t* deleteN1(Tree_t* root, int value);
+Tree_t* findN(Tree_t* root, int value);
+void print(Tree_t* root);
+

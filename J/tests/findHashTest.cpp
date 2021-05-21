24aa796dba141ee4ca233b629bb8a28e26833634
lyphrowny <strukov.as@edu.spbstu.ru>
2021-05-21 18:17:54 +0300

    added exhaustive test

--- J/tests/findHashTest.cpp
+++ J/tests/findHashTest.cpp
@@ -12,7 +12,7 @@
 class FindHash : public testing::Test {
 protected:
     ht_t* table = nullptr;
-    const int capacity = 4;
+    const int capacity = 0;
 
     inline void _createHash(int _capacity) { table = createHash(table, _capacity); }
 
@@ -20,84 +20,37 @@ protected:
 
     ~FindHash() override { freeHash(table); }
 
-    void insertHashes(const std::vector<std::string> &vec) {
-        for (auto &elem: vec)
+    void insertHashes(const std::set<std::string> &set) {
+        for (auto &elem: set)
             table = insertHash(table, elem.c_str());
     }
 
-    void deleteHashes(const std::vector<std::string> &vec) {
-        for (auto &elem: vec)
+    void deleteHashes(const std::set<std::string> &set) {
+        for (auto &elem: set)
             table = deleteHash(table, elem.c_str());
     }
 
-
-    static void
-    checkNodes(ht_t* root, const std::vector<std::string> &nodes, const std::unordered_set<std::string> &set,
-               const std::vector<std::string> &vec) {
-        for (const auto &node_val : nodes) {
-            if (set.count(node_val) > 0) // node_val in set
-                EXPECT_NE(findHash(root, node_val.c_str()), nullptr)
-                                    << node_val << " Range: " << testing::PrintToString(nodes)
-                                    << " Inserted: " << testing::PrintToString(vec)
-                                    << " Set: " << testing::PrintToString(set);
-            else
-                EXPECT_EQ(findHash(root, node_val.c_str()), nullptr)
-                                    << node_val << " Range: " << testing::PrintToString(nodes)
-                                    << " Inserted: " << testing::PrintToString(vec)
-                                    << " Set: " << testing::PrintToString(set);
-        }
+    void findHashes(const std::set<std::string> &set, bool shouldBe) {
+        for (auto &elem: set)
+            ASSERT_EQ(findHash(table, elem.c_str()) != nullptr, shouldBe) << elem << " " << shouldBe << std::endl;
     }
 
-    static void checkNodes(ht_t* root, const std::vector<std::string> &all_vals,
-                           const std::unordered_set<std::string> &inserted_set,
-                           const std::vector<std::string> &inserted_vec,
-                           const std::unordered_set<std::string> &deleted_set,
-                           const std::vector<std::string> &deleted_vec) {
-        for (const auto &node_val : all_vals) {
-            if (inserted_set.count(node_val) > 0) // node_val in set
-                EXPECT_NE(findHash(root, node_val.c_str()), nullptr)
-                                    << node_val << " Range: " << testing::PrintToString(all_vals)
-                                    << " Inserted Set: "
-                                    << testing::PrintToString(inserted_set)
-                                    << " Inserted Vec: "
-                                    << testing::PrintToString(inserted_vec) << std::endl
-                                    << " Deleted Set: " << testing::PrintToString(deleted_set)
-                                    << " Deleted Vec: " << testing::PrintToString(deleted_vec);
-            else
-                EXPECT_EQ(findHash(root, node_val.c_str()), nullptr)
-                                    << node_val << " Range: " << testing::PrintToString(all_vals)
-                                    << " Inserted Set: "
-                                    << testing::PrintToString(inserted_set)
-                                    << " Inserted Vec: "
-                                    << testing::PrintToString(inserted_vec) << std::endl
-                                    << " Deleted Set: " << testing::PrintToString(deleted_set)
-                                    << " Deleted Vec: " << testing::PrintToString(deleted_vec);
-        }
+    void findHashes(const std::string &hash, bool shouldBe) {
+        ASSERT_EQ(findHash(table, hash.c_str()) != nullptr, shouldBe) << hash << " " << shouldBe << std::endl;
     }
 
-    void randomFind(int number_of_nodes, int number_of_deletions) {
-        std::unordered_set<std::string> nodes, deleted;
-        std::vector<std::string> all_nodes(number_of_nodes), inserted_nodes, deleted_nodes;
-        std::generate(all_nodes.begin(), all_nodes.end(), [n = 0]()mutable { return n++; });
+    std::string genString(int size) {
+        std::string str(
+                "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
         std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> insert_val(0, number_of_nodes - 1), delete_val(0, number_of_deletions - 1);
-        auto insert = std::bind(insert_val, gen), del = std::bind(delete_val, gen);
+        std::mt19937 generator(rd());
 
-        for (int i = 0; i < number_of_nodes; i++) {
-            inserted_nodes.push_back(std::to_string(insert()));
-            nodes.insert(inserted_nodes.back());
-            table = insertHash(table, inserted_nodes.back().c_str());
-            checkNodes(table, all_nodes, nodes, inserted_nodes);
-        }
-        for (int i = 0; i < number_of_deletions; i++) {
-            deleted_nodes.push_back(std::to_string(del()));
-            deleted.insert(deleted_nodes.back());
-            table = deleteHash(table, deleted_nodes.back().c_str());
-            nodes.erase(deleted_nodes.back());
-            checkNodes(table, all_nodes, nodes, inserted_nodes, deleted, deleted_nodes);
-        }
+        std::shuffle(str.begin(), str.end(), generator);
+
+        return str.substr(0, size);
     }
+
 };
 
 TEST_F(FindHash, 1Elem) {
@@ -269,11 +222,35 @@ TEST_F(FindHash, 3Elem_EqHash_1rDel) {
 }
 
 TEST_F(FindHash, Exhaustive) {
-    for (int number_of_nodes = 0; number_of_nodes < 35; number_of_nodes++)
-        for (int i = 0; i < 2000; i++)
-            for (int number_of_deletions = 0; number_of_deletions < number_of_nodes * 2; number_of_deletions++) {
-                randomFind(number_of_nodes, number_of_deletions);
-                freeHash(table);
-                table = nullptr;
-            }
+    int v_size = 7000, s_size = 100;
+    std::set<std::string> to_insert, to_delete, common, diff, common_copy;
+    for (int i = 0; i < v_size; i++) {
+        to_insert.insert(genString(s_size));
+        to_delete.insert(genString(s_size));
+        common.insert(genString(s_size));
+    }
+    std::set_intersection(to_insert.begin(), to_insert.end(), to_delete.begin(), to_delete.end(),
+                          std::inserter(diff, diff.begin()));
+    if (!diff.empty())
+        for (auto &d : diff)
+            to_delete.erase(d);
+
+    insertHashes(to_insert);
+    findHashes(to_insert, true);
+    deleteHashes(to_delete);
+    findHashes(to_insert, true);
+    findHashes(to_delete, false);
+    insertHashes(common);
+
+    std::copy(common.begin(), common.end(), std::inserter(common_copy, common_copy.begin()));
+    for (auto &hash : common_copy) {
+        findHashes(common, true);
+        common.erase(hash);
+        table = deleteHash(table, hash.c_str());
+        findHashes(hash, false);
+    }
+    insertHashes(common);
+    findHashes(common, true);
+    deleteHashes(common);
+    findHashes(common, false);
 }
